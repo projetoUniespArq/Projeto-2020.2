4ed738314f2d42e93e0e2a7d5e0224520265556d
yurifardel <yrnascimento@outlook.com>
2020-10-25 19:10:25 -0300

    init

---
+++ projeto/app.py
@@ -0,0 +1,19 @@
+import shelve
+
+def Login():
+  print('* Faça seu login *')
+  
+  nome  = input('Digite seu nome completo: ')
+  matricula = input('Digite sua matricula: ')  
+  
+  # banco de dados
+  db = shelve.open('./projeto/database.db')
+  db['Aluno'] = {
+    'nome': nome,
+    'matricula': matricula
+}
+  # manipulando valores
+  # print(db['Aluno']['nome'])
+
+  print(f'Bem vindo {nome}, login efetuado com sucesso')
+Login()
