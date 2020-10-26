8365befaa3476d5175d641b767493e83dab6c874
yurifardel <yrnascimento@outlook.com>
2020-10-25 21:37:04 -0300

    .

--- app.py
+++ app.py
@@ -1,19 +1,17 @@
 import shelve
 
 def Login():
-  print('* Faça seu login *')
+  print('* Faça seu acesso *')
   
   nome  = input('Digite seu nome completo: ')
-  matricula = input('Digite sua matricula: ')  
+  chaveAcesso = input('Digite sua chave de acesso: ')  
   
   # banco de dados
   db = shelve.open('database.db')
   db['Aluno'] = {
     'nome': nome,
-    'matricula': matricula
-}
-  # manipulando valores
-  # print(db['Aluno']['nome'])
+    'matricula': chaveAcesso
+  }
 
   print(f'Bem vindo {nome}, login efetuado com sucesso')
 Login()
