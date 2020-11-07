7fb085f7b694bf3a4e42787a26bae03ff48f2f8f
Victor França <v.franca077@gmail.com>
2020-11-07 10:48:55 -0300

    refiz o 1 e o 2 comando

--- projeto.py
+++ projeto.py
@@ -1,128 +1,99 @@
- 
 lcategoria = []
 ltematica = []
 llivro = []
 livro = dict()
-categoria = dict ()
 tematica = dict ()
-
 
 def menu ():
     print("Menu:")
     print("    1-Cadastro de categorias e temáticas")
     print("    2-Configurações de livros")
     print("    3-Editar quantidade de um titulo")
-    # print("    4-Excluir livros")
-    # print("    4-Busca por exemplares")
+    print("    4-Excluir livros")
+    print("    4-Busca por exemplares")
     print("    4-Importar informações de um livro por arquivo")
     print("    5-Status de livros")
     print("    6-Gerar relatorios")
     print('    7-Sair do sistema')
     opc = int(input('    Digite sua opção: '))
     return opc
-
-    
+  
 def cadastroCategoriasTematicas ():
+    
     print("-----Cadastro de Categorias e Temáticas------")
-    opc = int(input("Deseja cadastrar uma categoria nova?\n1-SIM\n2-Cadastro de Temáticas\n3-NÃO(Volta ao menu principal): ")) 
+    opc = int(input("Deseja cadastrar uma categoria nova?\n1-SIM\n2-Cadastro de Temáticas\n3-NÃO(Volta ao menu principal): "))
     while (opc != 4):
         if (opc==1):
-            categoria["Categoria"] = input("Digite a categoria: ").upper()
-            if categoria in lcategoria:
-                print("Essa categoria já esta cadastrada!")
-                main()
-            else:
-                lcategoria.append(categoria.copy())
+            categoria = input("Categoria: ")
+            if categoria not in lcategoria:
+                lcategoria.append(categoria)
                 print(lcategoria)
                 print("Categoria cadastrada com sucesso!")
-                cadastroCategoriasTematicas()
-                # main()           
-        elif (opc==2): 
-            cOrdenado=sorted(lcategoria,key=lambda l:l["Categoria"])
-            for i, j in enumerate (cOrdenado,start=1):
-                print(f'\n{i}-',end = ' ')
-                for k in j.values():
-                    print(f'{k}', end=' ')
-            numeroCategoria = int(input("\nInforme o numero da categoria: "))
-            tematica["Categoria"] = lcategoria[numeroCategoria-1]["Categoria"]
-            tematica["Tematica"]= input("Digite a tematica: ").upper()
+                main()
+            else: 
+                print("Categoria já foi cadastrada")               
+        elif (opc==2):
+            for i in range (len(lcategoria)):  
+                print(i+1,lcategoria[i])   
+            numeroCategoria = int (input("\nInforme o numero da categoria: ")) 
+            tematica=input("Tematica: ")
+            tematica = {
+                lcategoria[numeroCategoria-1]:tematica
+            }
             if tematica not in ltematica:
                 ltematica.append(tematica.copy())
                 print("Temática cadastrada com sucesso")
+                print(ltematica)
+                print(ltematica[0])
                 main()
             else:
                 print("Temática já cadastrada")               
         elif (opc==3):
-                main()    
-def configuracoesLivro():
+                main()
+def addLivro ():
     print("-----Cadastro de Livros------")
-    tOrdenado=sorted(ltematica,key=lambda l:(l["Categoria"],l["Tematica"]))
-    for i, j in enumerate (tOrdenado,start=1):
-        print(f'\n{i}-',end = ' ')
-        for k in j["Categoria"],j["Tematica"]:
-            print(f'{k} ',end = '')
+    for k,l in enumerate (ltematica):
+        for i,j in l.items():
+            print((k+1),"-",i+": "+str(j))
     newBook = int(input("\nDigite o numero correspondente a categoria e a tematica: "))
+    recebendoDados= ltematica[newBook-1]
+    recebendoCategoria= list(recebendoDados.keys())
+    recebendoTematica=list(recebendoDados.values())
     livro = {
         "Titulo": input("Informe o titulo: "),
         "Autor": input("Informe o Autor: "),
         "Ano": int(input("Digite o ano: ")),
         "Quantidade":int(input("Informe a quantidade de livros: ")),
-        "Assunto": input("Digite o Assunto: ")
-    }   
+        "Assunto": input("Digite o Assunto: "),
+        "Categoria":recebendoCategoria[0],
+        "Tematica":recebendoTematica[0]  
+    } 
     if livro not in llivro:
         llivro.append(livro.copy())
-        ltematica[newBook-1]["Livro"]=(llivro)
-              
-        print(ltematica)
         print(llivro)
-
-        arquivo = open('db.txt', 'a')
-        arquivo.write(f'COLLECTIONS: \n {ltematica}\n')
-
-
-        print('Digite 1 para remover livros: ', '\nDigite 2 para procurar livro' ,'\nDigite 3 para voltar ao menu: ')
-        value = int(input('Digite um valor: '))
-
-        if(value == 1):
-            print(f'Nome do livro: {livro["Titulo"]}')
-
-            x = input("Digite o nome do livro para ser deletado: ")
-            if(x == livro["Titulo"]):
-                del livro
-                print('Livro deletado com sucesso')
-                
-            main()
-
-        elif(value == 2):
-            print('*Buscar por exemplares')
-
-            lv = input('Digite o nome do livro: ')
-
-            if(lv == livro["Titulo"]):
-                print(livro)
-            else:
-                print('Esse livro não esta disponivel')
-
-            main()
-
-        else:
-            main()
-        
+        main()
     else:
         print("Livro já cadastrado")
 
-    
-        
-
-    
-
+def quantidadeLivros ():
+    nomeLivro= (input("Digite o nome do livro que deseja editar a quantidade: "))
+    for i in range (len(llivro)):
+        print(i)
+        print(len(livro))
+        if llivro[i]["Titulo"] == nomeLivro:
+            quantidade = int(input("Livro encontrado!\nAgora informe a quantidade: "))
+            llivro[i]["Quantidade"]=quantidade
+            print("Atulizada com sucesso!")
+            main()
+        elif i==len(llivro) and nomeLivro not in llivro[i]["Titulo"]:
+            print("Livro não encontrado")
 def main ():
     opcao=menu ()
     while True:
         case = {
             1: lambda: cadastroCategoriasTematicas(),
-            2: lambda: configuracoesLivro(),
-            3: lambda: print('third'),
+            2: lambda: addLivro(),
+            3: lambda: quantidadeLivros(),
             # 4: lambda: removeLivro(),
             # 4: lambda: buscarLivro(),
             4: lambda: print('sixth'),
